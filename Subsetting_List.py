bcb1594ec624c0a3c728bf1daafee0031837c5db
Kritika0220 <kritishah2017@gmail.com>
2020-05-29 22:51:53 +0530

    More on Lists

---
+++ Subsetting_List.py
@@ -0,0 +1,52 @@
+#________Subsetting Python lists  -
+
+x = ["a", "b", "c", "d"]
+x[1]
+x[-3] # same result!
+
+# Ex. 1:
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+#________Subset and calculate -
+x = ["a", "b", "c", "d"]
+print(x[1] + x[3])
+
+# Ex. 1:
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+#________Slicing and dicing
+#my_list[start:end]
+
+# Ex. 1:
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs = areas[:6]
+
+# Use slicing to create upstairs
+upstairs = areas[6:]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
+
